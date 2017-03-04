49a5acca0489adb81b015156587503e63f644ab8
Rick Van <x.rckvan@gmail.com>
2017-03-03 18:06:18 -0700

    using custome event listners

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,15 @@ import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
 import { DatabindingComponent } from './databinding/databinding.component';
+import { PropertyBindingComponent } from './databinding/property-binding.component';
+import { EventBindingComponent } from './databinding/event-binding.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     DatabindingComponent,
+    PropertyBindingComponent,
+    EventBindingComponent,
   ],
   imports: [
     BrowserModule,
